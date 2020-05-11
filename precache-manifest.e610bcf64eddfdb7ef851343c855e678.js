8600407504b1fed4f38593bd7b60cc40883f1853
Daniel Perez Alvarez <unindented@gmail.com>
2020-05-11 15:59:25 -0700

    feat: create empty React app

---
+++ precache-manifest.e610bcf64eddfdb7ef851343c855e678.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83871694c9aff9a1a156df86da98a560",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd58edd1486f9438cd67",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8457b1708c92d4bf52d",
+    "url": "/static/js/2.c03915a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c03915a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd58edd1486f9438cd67",
+    "url": "/static/js/main.1b8e1ef0.chunk.js"
+  },
+  {
+    "revision": "d2f3d15ed5812eb78419",
+    "url": "/static/js/runtime-main.92546905.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
